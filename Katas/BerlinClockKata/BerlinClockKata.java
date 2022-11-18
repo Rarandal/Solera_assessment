fc257fd12377d5a8d4233c9beb4449b08a77473e
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 10:00:27 +0100

    Created BerlinClockData and added main

---
+++ Katas/BerlinClockKata/BerlinClockKata.java
@@ -0,0 +1,15 @@
+package Katas.BerlinClockKata;
+
+import java.util.*;
+
+public class BerlinClockKata {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("introduce una fecha: ");
+        String readData = s.nextLine();
+
+        System.out.println(readData);
+
+    }
+}
