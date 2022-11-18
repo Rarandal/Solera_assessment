784c804ded3b487b6f06ea475fb4302d68784e2f
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 15:25:58 +0100

    debug Problem1,2

---
+++ BugHunting/Problem2/Problem2.java
@@ -0,0 +1,17 @@
+/**
+ * Base bugged
+ */
+@override
+public double[] computeMean(){
+        // Not supported except for singletons.
+        return points.size() == 1 ? points.get(1) : null;
+        }
+
+/**
+ * Solved state
+ */
+@override class
+public double computeMean(){
+        // Not supported except for singletons.
+        return points.size() == 1 ? points.get(1) : -1;
+        }
