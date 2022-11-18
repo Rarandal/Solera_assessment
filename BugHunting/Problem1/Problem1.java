d7f96f76007e4c66ea74f7f0c57b0ecc6cf65e62
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:38:51 +0100

    Added Problem1.java base problem

--- BugHunting/Problem1/Problem1.java
+++ BugHunting/Problem1/Problem1.java
@@ -1,2 +1,16 @@
+/*
+Base Bugged snippet
 
-
+public class Problem1 {
+    private boolean checkDimensions(CLIQUEUnit other, int e) {
+        for (int i = 0; i < e; i++, j += 2) {
+            if (dims[i] != other.dims[i]
+                    || bounds[j] != other.bounds[j]
+                    || bounds[j + 1] != bounds[j + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+*/
