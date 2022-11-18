fd3e9b64c05fa88df7611df8376f29ca36b86880
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 14:53:13 +0100

    Problem1 debug

--- BugHunting/Problema1/Problem1.java
+++ BugHunting/Problema1/Problem1.java
@@ -1,11 +1,11 @@
 package BugHunting.Problema1;
 
 public class Problem1 {
-    private boolean checkDimensions(CLIQUEUnit other, int e) {
-        for (int i = 0; i < e; i++, j += 2) {
-            if (dims[i] != other.dims[i]
-                    || bounds[j] != other.bounds[j]
-                    || bounds[j + 1] != bounds[j + 1]) {
+    private boolean checkDimensions(CLIQUEUnit one, CLIQUEUnit other, int e) {
+        for (int i = 0, j = 0; i < e; i++, j += 2) {
+            if (one.dims[i] != other.dims[i]
+                    || one.bounds[j] != other.bounds[j]
+                    || one.bounds[j + 1] != other.bounds[j + 1]) {
                 return false;
             }
         }
