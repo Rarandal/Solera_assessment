9c7fa9a8a4ccdd6fe235b54f5d1fad2c6b9a674e
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:40:33 +0100

    added Problem2.java solution

--- BugHunting/Problem2/Problem2.java
+++ BugHunting/Problem2/Problem2.java
@@ -7,3 +7,11 @@ public double[] computeMean(){
         return points.size() == 1 ? points.get(1) : null;
         }
 
+/**
+ * Solved state
+ */
+@override class
+public double computeMean(){
+        // Not supported except for singletons.
+        return points.size() == 1 ? points.get(1) : -1;
+        }
