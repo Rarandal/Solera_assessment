2d3114978d7d567cdc023dd98e37425ddfb7c819
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:51:20 +0100

    added ExtraProblem.java solution

--- BugHunting/ExtraProblem/ExtraProblem.java
+++ BugHunting/ExtraProblem/ExtraProblem.java
@@ -22,5 +22,29 @@ public boolean equals (Object o)
         Objects.equals( checkpointStatisticsPerTask, that.checkpointStatisticsPerTask) ;
         }
 
+/**
+ * Solved
+ */
+@Override
+public boolean equals (Object o)
+        CheckpointStatistics that = (CheckpointStatistics) o;
+        if(id == that.id &&
+        savepoint == that.savepoint &&
+        triggerTimestamp == that.triggerTimestamp &&
+        latestAckTimestamp == that.latestAckTimestamp &&
+        stateSize == that.stateSize &&
+        duration == that.duration &&
+        alignmentBuffered == that.alignmentBuffered &&
+        processedData == that.processedData &&
+        persistedData == that.persistedData &&
+        numSubtasks == that.numSubtasks &&
+        numAckSubtasks == that.numAckSubtasks &&
+        status == that.status &&
+        Objects.equals(checkpointType, that.checkpointType) &&
+        Objects.equals( checkpointStatisticsPerTask, that.checkpointStatisticsPerTask)){
+        return true;
+        }else{
+        return false;
+        };
 
-
+        }
