eadea8377e217d05ebfebbb84d0a23bf681ac848
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:21:22 +0100

    debug Problem3 and Expra project

---
+++ BugHunting/Problem3/Problem3.java
@@ -0,0 +1,25 @@
+/**
+ * Bugged project
+ */
+
+protected PreDeConModel computeLocalModel (DoubleDBIDList neightbords, ....) {
+    final int referenceSetSize = neightbord.size();
+        ....
+        //Shouldn't happend
+        if (referenceSetSize < 0) {
+            LOG.warning("Empty reference set -"
+        "should at least include the query point!");
+            return new PreDeConModel(Integer.MAX_VALUE, DBIDUtil.EMPTYDBIDS);
+        }
+        int[] s;
+        ArrayList<Double> mvVar = new ArrayList<Doubled>();
+        for(int d = 0; d < dim; d++) {
+            s[d] /= referenceSetSize;
+            mvVar.put(s[d]);
+        }
+        ....
+}
+
+/**
+ * Debuged
+ */
