0540b1a8143dc3dcf4663dc3be5158c93e872ec2
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:41:54 +0100

    added Problem3.java solution

--- BugHunting/Problem3/Problem3.java
+++ BugHunting/Problem3/Problem3.java
@@ -19,3 +19,21 @@ final int referenceSetSize = neightbord.size();
         ....
         }
 
+/**
+ * Debuged
+ */
+
+protected PreDeConModel computeLocalModel (DoubleDBIDList neightbords, ....) {
+final int referenceSetSize = neightbord.size();
+        //Shouldn't happend
+        if (referenceSetSize < 0) {
+        logger.warning("Empty reference set - should at least include the query point!");
+        return new PreDeConModel(Integer.MAX_VALUE, DBIDUtil.EMPTYDBIDS);
+        }
+        int[] s;
+        for(int d = 0; d < dim; d++) {
+        s[d] /= referenceSetSize;
+        mvVar.put(s[d]);
+        }
+        return new PreDeConModel(referenceSetSize, s[d])
+        }
