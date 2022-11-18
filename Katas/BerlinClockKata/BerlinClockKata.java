6720c7ba6953e6856bc531690401b1acd9ce891e
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 11:10:15 +0100

    Mostrar 1minutes and 5minutes

--- Katas/BerlinClockKata/BerlinClockKata.java
+++ Katas/BerlinClockKata/BerlinClockKata.java
@@ -5,11 +5,60 @@ import java.util.*;
 public class BerlinClockKata {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        /*Scanner s = new Scanner(System.in);
         System.out.print("introduce una fecha: ");
-        String readData = s.nextLine();
+        String readData = s.nextLine();*/
 
-        System.out.println(readData);
+        String[] fecha = {"00:00:00", "23:59:59", "12:04:00", "12:23:00", "12:35:00"};
 
+        for (int i = 0; i < 5; i++) {
+            String[] splitted;
+            //splitted = radData.trim().split(":");
+            splitted = fecha[i].trim().split(":");
+
+            //int hours = Integer.parseInt(splitted[0]);
+            int minutes = Integer.parseInt(splitted[1]);
+            //int seconds = Integer.parseInt(splitted[2]);
+
+            int moduloMinute = minutes % 5;
+            //Mostrar1Minutes(moduloMinute);
+
+            int moduloMinutes = minutes / 5;
+            //Mostrar5Minutes(moduloMinutes);
+
+        }
+    }
+
+    public static void Mostrar1Minutes(int encendidos) {
+        switch (encendidos) {
+            case 0:
+                mostrar("0000");
+                return;
+            case 1:
+                mostrar("Y000");
+                return;
+            case 2:
+                mostrar("YY00");
+                return;
+            case 3:
+                mostrar("YYY0");
+                return;
+            case 4:
+                mostrar("YYYY");
+                return;
+        }
+    }
+
+    public static void Mostrar5Minutes(int encendidos) {
+        String casa = "OOOOOOOOOOO";
+        String perro = "YYRYYRYYRYY";
+        String result = perro.substring(0, encendidos) + casa.substring(encendidos, casa.length());
+
+        mostrar(result);
+    }
+
+    public static void mostrar(String casa) {
+
+        System.out.println(casa);
     }
 }
