74d280a6e14bd356449c79720abe5b347c787282
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:39:14 +0100

    Added Problem1.java solution

--- BugHunting/Problem1/Problem1.java
+++ BugHunting/Problem1/Problem1.java
@@ -14,3 +14,20 @@ public class Problem1 {
     }
 }
 */
+
+/*
+ * Solved Snippet
+ * */
+
+public class Problem1 {
+    private boolean checkDimensions(CLIQUEUnit one, CLIQUEUnit other, int e) {
+        for (int i = 0, j = 0; i < e; i++, j += 2) {
+            if (one.dims[i] != other.dims[i]
+                    || one.bounds[j] != other.bounds[j]
+                    || one.bounds[j + 1] != other.bounds[j + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
