bb72725fc7f2ec05755808ca7d3ce40b2c189f35
Roberto.Aranda <Robertoaranda133@gmail.com>
2022-11-18 16:50:59 +0100

    added ExtraProblem.java base problem

---
+++ BugHunting/ExtraProblem/ExtraProblem.java
@@ -0,0 +1,26 @@
+/**
+ * Bugged project
+ */
+
+@Override
+public boolean equals (Object o)
+        {
+        CheckpointStatistics that = (CheckpointStatistics) o;
+        return id == that.id &&
+        savepoint == that.savepoint &&
+        triggerTimestamp == that.triggerTimestamp &&
+        latestAckTimestamp == that.latestAckTimestamp &&
+        stateSize == that.stateSize &&
+        duration == that.duration &&
+        alignmentBuffered == that.alignmentBuffered &&
+        processedData == processedData &&
+        persistedData == that.persistedData &&
+        numSubtasks == that.numSubtasks &&
+        numAckSubtasks == that.numAckSubtasks &&
+        status == that.status &&
+        Objects.equals(checkpointType, that.checkpointType) &&
+        Objects.equals( checkpointStatisticsPerTask, that.checkpointStatisticsPerTask) ;
+        }
+
+
+
